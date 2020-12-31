46bcddd9fe5f055dc3e553981bc3213653e375a8
dranixxxx <phoang0302@gmail.com>
2020-12-31 12:44:33 +0700

    first

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a1db914e5dfc1bb02740cdb37f0b22c2'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c1adc73c-d829-4a22-aa5d-31fd020e4e44'
+};
